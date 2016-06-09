b1d5b29d1529a3fe776a72ac38a9654932b30823
Matthew Power <matthew.power@hogarthww.com>
2016-06-09 21:33:29 +0100

    second pass

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import functools
 import networkx as nx
-from networkx.exception import NetworkXNoPath
+from networkx.exception import NetworkXError
 
 PAGES_SCRAPED = 0
 
@@ -11,6 +11,7 @@ URL_TEMPlATE = 'https://en.wikipedia.org/wiki/{}'
 
 @functools.lru_cache(maxsize=None)
 def scrape_page(page):
+    print(page)
     response = requests.get(URL_TEMPlATE.format(page))
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -37,26 +38,41 @@ def scrape_page(page):
 def wiki_distance(start_page_name, end_page_name):
     depth = 0
     G = nx.Graph()
+    articles = scrape_page(start_page_name)
+    global PAGES_SCRAPED
+    PAGES_SCRAPED += 1
+    print(PAGES_SCRAPED)
+
+    for article in articles:
+        G.add_edge(start_page_name, article)
+
     while True:
-        articles = scrape_page(start_page_name)
-
-        for article in articles:
-            G.add_edge(start_page_name, article)
-
         try:
             shortest_path = nx.shortest_path(
                 G, source=start_page_name, target=end_page_name
             )
-        except NetworkXNoPath:
+            break
+        except NetworkXError:
             print('No path found, continuing...')
             print(depth)
             depth += 1
-            continue
 
-        length = len(shortest_path) - 1
-        print('The shortest path is {}, and the distance is {}'.format(str(shortest_path), length))
-        return length
+        for article_boundary in nx.node_boundary(G, [start_page_name]):
+
+            new_articles = scrape_page(article_boundary)
+            for new_article in new_articles:
+                G.add_edge(article_boundary, new_article)
+
+        print('============= Next iteration')
+
+    length = len(shortest_path) - 1
+    print(
+        'The shortest path is {}, and the distance is {}'.format(
+            str(shortest_path), length
+        )
+    )
+    return length
 
 
 if __name__ == '__main__':
-    wiki_distance('Jesus', 'Christianity')
+    wiki_distance('Bank_of_America', 'Jesus')
